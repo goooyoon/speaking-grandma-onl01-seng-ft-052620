4102be3ee8762fe624db692f57bd04e0622ca0ee
Grace Kim <goooyoon@gmail.com>
2020-05-27 00:55:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
-
-def speak_to_grandma(string)
-  if string == "I LOVE YOU GRANDMA!"
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-    elsif string == string.upcase
-   "NO, NOT SINCE 1938!"
+  elsif phrase == phrase.upcase
+    "NO, NOT SINCE 1938!"
   else
-     "HUH?! SPEAK UP, SONNY"
+    "HUH?! SPEAK UP, SONNY!"
+  end
 end
